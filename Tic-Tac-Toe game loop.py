c97abc7f881f4c4b60f516f12b23643228e21ad9
AshfakZaman <76805477+AshfakZaman@users.noreply.github.com>
2024-06-10 01:19:54 +0600

    Update Tic-Tac-Toe game loop.py

--- Tic-Tac-Toe game loop.py
+++ Tic-Tac-Toe game loop.py
@@ -1,3 +1,4 @@
+
 from Tic_Tac_Toe_All_Functions import *
 
 print("Welcome to Tic-Tac-Toe game")
@@ -9,48 +10,48 @@ while True:
     while True:
         while True:
             if current_player == 1:
-                player_1_code()
-                if player_1_win_check():
-                    
+                player_1_code(board,board_with_numbers,available_positions)
+                if player_1_win_check(board):
+
                     break
                 elif to_check_blank_space(board) == False:
                     break
                 current_player = 2
             else:
-                player_2_code()
-                if player_2_win_check():
-    
+                player_2_code(board, board_with_numbers,available_positions)
+                if player_2_win_check(board):
+
                     break
                 elif to_check_blank_space(board) == False:
                     break
                 current_player = 1
 
-        if player_1_win_check():
+        if player_1_win_check(board):
             print("\n" * 1)
-            to_display_board()
+            to_display_board(board,board_with_numbers)
             print("Player 1 is the Winner !")
             break
-                  
-        elif player_2_win_check():
+
+        elif player_2_win_check(board):
             print("\n" * 1)
-            to_display_board()
+            to_display_board(board,board_with_numbers)
             print("Player 2 is the Winner !")
             break
-        
-    
-        elif not player_1_win_check() and not player_2_win_check():
+
+
+        elif not player_1_win_check(board) and not player_2_win_check(board):
             print("\n" * 1)
-            to_display_board()
+            to_display_board(board,board_with_numbers)
             print("No one wins.it's a TIE")
             break
-    
-            
+
     if replay() == "y":
         board = list(" ") * 10
-        board_with_numbers = [0,"1","2","3","4","5","6","7","8","9"]
+        board_with_numbers = [0, "1", "2", "3", "4", "5", "6", "7", "8", "9"]
         available_positions = list(range(1, 10))
+        #to_display_board(board,board_with_numbers)
+        current_player = 1
         continue
     else:
         print("Ashfak says..thanks for playing")
         break
-
